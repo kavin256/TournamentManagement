44006393730d6e585321d29afc13ade50e8909d2
Kavin Ranawella <ranawella.k@gmail.com>
2016-11-22 10:29:08 +0530

    Add files via upload

--- enterDB.php
+++ enterDB.php
@@ -1,8 +1,8 @@
-<html>
 
 <!-- Connecting to the database -->
 
 <?php
+error_reporting(0);
 
 //
 $NArr = array($_POST['team1'] , $_POST['team2'], $_POST['team3'] , $_POST['team4'] , $_POST['team5'] , $_POST['team6'], $_POST['team7'] , $_POST['team8'] , $_POST['team9'] , $_POST['team10'], $_POST['team11'] , $_POST['team12'] , $_POST['team13'] , $_POST['team14'], $_POST['team15']);
@@ -24,10 +24,14 @@ for ($i = 0 ; $NArr[$i] != null ; $i++){
 	else {
 	    echo "Error: " . $sql . "<br>" . $conn->error;
 	}
+
 }
 
+echo "The ".$i." teams have been successfully submitted !";
+
+	
 $conn->close();
 
-?>
 
-</html>
+
+?>
